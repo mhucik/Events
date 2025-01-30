d46423bdaf1fbf34ea440e29907bc5a26b34140c
Marek Hučík <marek.hucik@reservio.com>
2025-01-30 14:28:08 +0100

    Fix: PHP 8.3 compatibility

--- src/Events/LazyEventManager.php
+++ src/Events/LazyEventManager.php
@@ -50,7 +50,7 @@ class LazyEventManager extends \Kdyby\Events\EventManager
 	/**
 	 * {@inheritdoc}
 	 */
-	public function getListeners($eventName = NULL)
+    public function getListeners(string $eventName = NULL): array
 	{
 		if ($eventName === NULL) {
 			while (($type = key($this->listenerIds)) !== NULL) {
@@ -67,7 +67,7 @@ class LazyEventManager extends \Kdyby\Events\EventManager
 	/**
 	 * {@inheritdoc}
 	 */
-	public function removeEventListener($unsubscribe, $subscriber = NULL)
+    public function removeEventListener(string|array $unsubscribe, object $subscriber = NULL): void
 	{
 		if ($unsubscribe instanceof EventSubscriber) {
 			[$unsubscribe, $subscriber] = $this->extractSubscriber($unsubscribe);
